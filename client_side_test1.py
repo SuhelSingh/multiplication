1bd92e0fc79d6cbfa8b895d6b1963c6ae10c2ee8
Chris J <christopher.w.jarvis10@gmail.com>
2016-05-09 23:59:23 -0500

    Added landing page, added practice mode functionality

--- client_side_test1.py
+++ client_side_test1.py
@@ -4,31 +4,21 @@ import time
 import multiplication as m
 
 # holds starting and ending time of each problem
-# eventually move all timing to client side but this is easier for a prototype
-
 timing_session = []
 session_data = []
 m.initialize()
 
-# *** landing page to start test ***
 app = Flask(__name__)
-@app.route('/')
-def test():
-    return render_template('timedtest1.html')
-# *** sends 2 numbers back to client in response to json request, writes initial
-# start time of each problem to list ***
+# ***** ANSWER SENDING AND RECEIVING FUNCTIONS FOR ALL TESTS *****
+# sends 2 numbers to client
 @app.route('/newNumbers/')
 def newNumbers():
-    # want to flash message here, correct or incorrect
     timing_session.append(time.clock())
     (a,b) = m.selectNumbers()
     response = make_response('{"1": %d, "2": %d}' % (a,b))
     response.headers['Content-Type'] = 'application/json'
     print 'sent new numbers'
     return response
-# *** subtracts current time from start time, recieves problem attempted and
-# whether or not it was correct, adds time to this, adds this dict to session_data
-# list ***
 @app.route('/receiveAnswers/', methods=["POST"])
 def receiveAnswers():
     timer = time.clock() - timing_session[-1]
@@ -37,8 +27,7 @@ def receiveAnswers():
     session_data.append(data)
     print 'recieved answers'
     return ('',204)
-# *** after timer in client exits test, this is pinged to write session_data to
-# a file, the page is redirected back to test start from the client-side ***
+# writes data for each problem to a dataframe
 @app.route('/sendAnswers/')
 def sendAnswers():
     # flash message test completed, results too eventually
@@ -47,13 +36,31 @@ def sendAnswers():
     del session_data[:]
     print 'sendAnswers pinged, writing data to file'
     return ('',204)
-# this function sends all of the aggregated session datas to the backend
+# when user is done, send all sessions to server
 @app.route('/DataToServer/')
 def data_to_server():
     print 'all session datas sent to server'
     return ('',204)
     # m.send_all()
+# ***** END SHARED FUNCTIONS *****
 
+# ***** START TIMED TEST ******
+@app.route('/timedTest/')
+def timed_test():
+    return render_template('timedtest1.html')
+# ***** END TIMED TEST *****
+
+# ***** START PRACTICE MODE *****
+@app.route('/practice/')
+def practice():
+    return render_template('practicemode1.html')
+# ***** END PRACTICE MODE *****
+
+# *** landing page to choose test***
+@app.route('/')
+def landing():
+    # eventually this will have a login page also
+    return render_template('landingpage1.html')
 
 if __name__=='__main__':
     app.run(debug=True)
